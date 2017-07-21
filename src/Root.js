12f006031da73084ff6cf578920c6b00906a1d92
Connor Campbell <connorcampbell@connors-mbp.infotronics.com>
2017-07-21 13:18:04 -0400

    Add newer versions of react-router and react-router-redux

--- src/Root.js
+++ src/Root.js
@@ -1,37 +1,40 @@
 import React from 'react'
 
+/*Redux Utilites*/
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
+/*Routing Utilities*/
+import { Router, Route, IndexRoute } from 'react-router'
+import { createBrowserHistory } from 'history'
+import { ConnectedRouter, routerReducer } from 'react-router-redux'
 
-import ReactRouterTest from './components/react-router-test'
-
+/*Reducers*/
 import reducers from './reducers/index'
 
+/*Containers*/
 import { App, Profile, ProfileSnapShot } from './containers/index'
 
-const combinedReducers = combineReducers({
-  ...reducers,
-  routing: routerReducer
-})
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  })
+)
 
-const store = createStore(combinedReducers , window.STATE_FROM_SERVER)
-
-const history = syncHistoryWithStore(browserHistory, store)
+const history = createBrowserHistory()
 
 function Root() {
   return (
     <Provider store={store}>
-      <Router history={history}>
+      <ConnectedRouter history={history}>
         <Route path="/" component={App}>
           <IndexRoute component={Profile}>
             <IndexRoute component={ProfileSnapShot}>
             </IndexRoute>
           </IndexRoute>
         </Route>
-      </Router>
+      </ConnectedRouter>
     </Provider>
   )
 }
