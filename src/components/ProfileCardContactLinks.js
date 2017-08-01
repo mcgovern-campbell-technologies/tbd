c7b1d960972bc1b590fea085fa25350224f7f00d
Connor Campbell <connorcampbell@connors-mbp.local>
2017-08-01 12:48:26 -0400

    Add completed ProfileCard

--- src/components/ProfileCardContactLinks.js
+++ src/components/ProfileCardContactLinks.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Icon, Row } from 'react-materialize'
 
 function ProfileCardContactLinks () {
   return (
-    <p>ProfileCardContactLinks</p>
+    <div>
+      <p><Icon tiny>mail</Icon> rjohnson84@gmail.com</p>
+      <p><Icon tiny>location_on</Icon> Warren, MI</p>
+      <p><Icon tiny>link</Icon> rjtech84</p>
+    </div>
+
   )
 }
 
