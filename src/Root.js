8f6d2b3b46d9f7d754f7f7481de0ca22bfc6edbc
Luke Wilson <wilson.palooza@gmail.com>
2017-10-14 12:45:06 -0700

    remove Switch component and just wrap Route components in a div (following documentation)

--- src/Root.js
+++ src/Root.js
@@ -5,7 +5,7 @@ import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 /* Routing Utilities */
-import { Router, Route, IndexRoute, Switch } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
 import { createBrowserHistory } from 'history'
 import { ConnectedRouter, routerReducer } from 'react-router-redux'
 import * as reactRouterRedux from 'react-router-redux'
@@ -29,11 +29,11 @@ function Root() {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Switch>
+        <div>
           <Route exact path="/" component={() => <div>hello world</div>}>
           </Route>
           <Route path="/test" component={Test}></Route>
-        </Switch>
+        </div>
       </ConnectedRouter>
     </Provider>
   )
