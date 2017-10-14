b3426d7be7a4f8f3b32a40549a33d5e6a9900f94
Luke Wilson <wilson.palooza@gmail.com>
2017-10-14 12:32:01 -0700

    create components directory and Test component

---
+++ src/components/test.js
@@ -0,0 +1,7 @@
+// This component just used for testing.
+// Not sure if having components in this directory is correct. Need to check with team. - Luke.
+import React from 'react';
+
+export default () => (
+  <h1>This is a test</h1>
+)
