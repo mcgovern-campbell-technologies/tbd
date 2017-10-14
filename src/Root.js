3bfeef9e2006420e5658057ca9602a144208ea14
Luke Wilson <wilson.palooza@gmail.com>
2017-10-14 12:29:07 -0700

    wrap Route components in a Switch component (from React Router v4); add /test route

--- src/Root.js
+++ src/Root.js
@@ -5,7 +5,7 @@ import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 /* Routing Utilities */
-import { Router, Route, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute, Switch } from 'react-router'
 import { createBrowserHistory } from 'history'
 import { ConnectedRouter, routerReducer } from 'react-router-redux'
 import * as reactRouterRedux from 'react-router-redux'
@@ -15,6 +15,9 @@ import configureStore from './redux/configureStore'
 
 /*Containers*/
 import { } from './containers/index'
+
+/*Components*/
+import Test from './components/test';
 
 /* create store */
 const store = configureStore()
@@ -26,8 +29,11 @@ function Root() {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Route exact path="/" component={() => <div>hello world</div>}>
-        </Route>
+        <Switch>
+          <Route exact path="/" component={() => <div>hello world</div>}>
+          </Route>
+          <Route path="/test" component={Test}></Route>
+        </Switch>
       </ConnectedRouter>
     </Provider>
   )
