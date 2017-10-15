868fb4ac2d2b09919dccac13b517d82120b937ef
Luke Wilson <wilson.palooza@gmail.com>
2017-10-15 08:24:47 -0700

    remove console.logs

--- src/redux/modules/example.js
+++ src/redux/modules/example.js
@@ -1,6 +1,4 @@
 const exampleEpic = action$ => {
-  console.log('action$ from exampleEpic')
-  console.log(action$)
   return action$.ofType('EXAMPLE')
     .delay(1000)
     .mapTo({ type: 'EXAMPLE_FULLFILLED'})
@@ -10,13 +8,10 @@ const exampleReducer = (state = { example: false }, action) => {
 
   switch (action.type) {
     case 'EXAMPLE':
-      console.log(state);
       return { example: true }
     case 'EXAMPLE_FULLFILLED':
-      console.log(state);
       return { example: false }
     default:
-      console.log(state);
       return state
   }
 }
