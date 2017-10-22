7b31d7cc65e72247f82c2661ffab575fec2c6173
Connor Campbell <connorcampbell@connors-mbp.local>
2017-10-22 17:12:38 -0400

    Add the proper boiler plate for redux-form

--- src/redux/modules/root.js
+++ src/redux/modules/root.js
@@ -2,6 +2,7 @@ import { combineEpics } from 'redux-observable'
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux';
 
+import { reducer as formReducer } from 'redux-form'
 /*
   import epics and reducers here,
   they should be kept in the same file
@@ -9,6 +10,7 @@ import { routerReducer } from 'react-router-redux';
 */
 import exampleReducer, { exampleEpic } from './example'
 import authReducer, { authEpic } from './auth'
+import onBoardingReducer, { onBoardingEpic } from './onBoarding'
 
 export const rootEpic = combineEpics(
   /* epics */
@@ -20,7 +22,9 @@ export const rootReducer = combineReducers({
   /* reducers */
   exampleReducer,
   authReducer,
+  onBoardingReducer,
   one: (state = {}) => state,
   two: (state = {}) => state,
-  router: routerReducer
+  router: routerReducer,
+  form: formReducer
 })
