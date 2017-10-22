3ac929f111133ac222440f30e9a4e1751d8f3a81
Connor Campbell <connorcampbell@connors-mbp.local>
2017-10-22 17:20:31 -0400

    Clean ContactForm component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,56 +1,6 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
-// export default class extends Component{
-
-//   constructor(props) {
-//     super(props)
-
-//     var { firstName, lastName, email, date, onBasicInfoUpdate } = this.props
-//     this.state = {
-//       firstName,
-//       lastName,
-//       email,
-//       date,
-//     }
-//     this.handleChange = this.handleChange.bind(this)
-//   }
-
-//   handleChange(e) {
-//     this.setState({[e.target.name]: e.target.value})
-//     this.props.onBasicInfoUpdate(e)
-//   }
-
-//   render() {
-//     const { firstName, lastName, email, date, onBasicInfoUpdate } = this.props
-//     return (
-//       <div>
-//         <form>
-//           <label>
-//             first name:
-//             <input type="text" name="firstName" value={firstName} onChange={this.handleChange}/>
-//           </label>
-//           <label>
-//             last name:
-//             <input type="text" name="lastName" value={lastName} onChange={(e) => onBasicInfoUpdate(e, "basicInfo")}/>
-//           </label>
-//           <label>
-//             email:
-//             <input type="email" name="email" value={email} onChange={(e) => onBasicInfoUpdate(e, "basicInfo")}/>
-//           </label>
-//           <label>
-//             date of birth:
-//             <input type="date"  value={date} onChange={(e) => onBasicInfoUpdate(e, "basicInfo")}/>
-//           </label>
-
-//           <input type="button" value="continue"/>
-//         </form>
-//       </div>
-//     )
-    
-//   }
-// }
-
 function ContactForm(props) {
   const { handleSubmit } = props
   return (
