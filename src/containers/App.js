c93e56e3cbfd571886fb72db7ba827b9e5bc0eb6
Connor Campbell <connorcampbell@connors-mbp.local>
2017-10-24 17:25:48 -0400

    Combine Main into App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,76 @@
-//infuse Main component with actionCreators
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React from 'react';
+
+/* Import Router Dependencies */
+import { Route, Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
+/* Prop Verification */
+import PropsRoute from '../utils/PropsRoute';
+import PropTypes from 'prop-types';
+
+/* Import Redux Utilities */
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import * as actionCreators from '../redux/actions/actionCreators';
-import Main from './Main';
+
+/* Import components */
+import { Test } from '../components/componentIndex';
+
+/* Import containers */
+import { OnBoardFlow, LandingPage } from './containerIndex'
+
+/* Import Auth utilities*/
+import AuthService from '../utils/AuthService';
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+    //instantiate the authService from class
+    this.authService = new AuthService();
+  }
+
+  componentWillMount() {
+    // Add callback for lock's `authenticated` event
+    this.authService.lock.on('authenticated', (authResult) => {
+      this.authService.lock.getProfile(authResult.idToken, (error, profile) => {
+        if (error) { return this.props.loginError(error); }
+        AuthService.setToken(authResult.idToken); // static method
+        AuthService.setProfile(profile); // static method
+        this.props.loginSuccess(profile);
+        return this.props.history.push({ pathname: '/' });
+      });
+    });
+    // Add callback for lock's `authorization_error` event
+    this.authService.lock.on('authorization_error', (error) => {
+      this.props.loginError(error);
+      return this.props.history.push({ pathname: '/' });
+    });
+  }
+
+  render() {
+    console.log('this.props from App')
+    console.log(this.props)
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/test">Login</Link></li>
+          </ul>
+        </nav>
+        <div>
+          <Route exact path="/" component={LandingPage} />
+          <PropsRoute path="/test" component={Test} authService={this.authService} {...this.props}/>
+          <Route path="/OnBoardFlow" component={OnBoardFlow}/>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+App.contextTypes = { store: PropTypes.object };
 
 const mapStateToProps = (state) => {
   return {
@@ -16,6 +82,5 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(actionCreators, dispatch);
 }
 
-const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
 
-export default App;
