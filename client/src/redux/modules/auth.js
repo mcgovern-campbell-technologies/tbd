1caaa936e5410114d58876a1926f9a54ba682d4b
Luke Wilson <wilson.palooza@gmail.com>
2017-10-26 23:20:11 -0700

    add console.logs to authReducer for debugging

--- client/src/redux/modules/auth.js
+++ client/src/redux/modules/auth.js
@@ -1,19 +1,19 @@
 import AuthService from '../../utils/AuthService'
 import * as types from '../../utils/types';
 
-//
+const authService = new AuthService();
+
 // const authEpic = action$ => {
-//   return action$.ofType('LOGIN_REQUEST')
-//     .delay(7000)
-//     .mapTo({ type: 'LOGIN_ERROR'})
 // }
 
 const authReducer = (state = {
-  isAuthenticated: !AuthService.isTokenExpired(),
+  // isAuthenticated: !AuthService.isTokenExpired(),
   isFetching: false,
-  profile: AuthService.getProfile(),
+  profile: authService.getProfile(),
   error: null,
 }, action) => {
+  console.log('action')
+  console.log(action)
   switch (action.type) {
     case types.LOGIN_REQUEST:
       console.log('firing LOGIN_REQUEST from auth.js module')
@@ -31,6 +31,7 @@ const authReducer = (state = {
         profile: action.profile,
       };
     case types.LOGIN_ERROR:
+      console.log('firing LOGIN_ERROR from auth.js module')
       return {
         ...state,
         isFetching: false,
@@ -39,6 +40,7 @@ const authReducer = (state = {
         error: action.error,
       };
     case types.LOGOUT_SUCCESS:
+    console.log('firing LOGOUT_SUCCESS from auth.js module')
       return {
         ...state,
         isAuthenticated: false,
