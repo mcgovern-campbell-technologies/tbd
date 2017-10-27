27b564fe0919593da0abecb601778e871db00fdc
Luke Wilson <wilson.palooza@gmail.com>
2017-10-26 22:26:06 -0700

    create Callback component for auth0 callback URL

---
+++ client/src/components/Callback.js
@@ -0,0 +1,19 @@
+// src/Callback/Callback.js
+
+import React, { Component } from 'react';
+// import loading from './loading.svg';
+
+class Callback extends Component {
+  render() {
+    // const style = //...
+
+    return (
+      <div>
+        <img src="" alt="loading"/>
+        <p>This is the callback URL for auth0</p>
+      </div>
+    );
+  }
+}
+
+export default Callback;
