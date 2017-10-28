1423fedb54185b902c7dd30b6c713e22276b1272
Connor Campbell <connorcampbell@connors-mbp.lan>
2017-10-28 12:27:21 -0400

    Add routers for more modular routes

---
+++ server/routers/employeeRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const jwtCheck = require('./../auth/auth').jwtCheck;
+
+// router.use(jwtCheck)
+router.get('/', (req, res) => {
+  
+})
+
+router.put('/', (req, res) => {
+  
+})
+
+module.exports = router
