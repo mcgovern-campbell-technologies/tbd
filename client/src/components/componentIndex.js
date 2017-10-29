cc8689540254264b1b4d6b7119c51381cb87d59e
Connor Campbell <connorcampbell@connors-mbp.local>
2017-10-29 19:22:16 -0400

    Add new components to the component index

--- client/src/components/componentIndex.js
+++ client/src/components/componentIndex.js
@@ -2,10 +2,16 @@ import ContactForm from './ContactForm'
 import LocationForm from './LocationForm'
 import Test from './Test'
 import Callback from './Callback'
+import ProfileSideCard from './ProfileSideCard'
+import ProfileCertifications from './ProfileCertifications'
+import ProfileSkills from './ProfileSkills'
 
 export {
   ContactForm,
   LocationForm,
   Test,
   Callback,
+  ProfileSideCard,
+  ProfileCertifications,
+  ProfileSkills
 }
