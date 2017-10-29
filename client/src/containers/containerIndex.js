b3b4a315bcab070442f6038a38a117db61e3ca95
Jennica Goo <jennica@containn.com>
2017-10-28 23:42:13 -0700

    Add initial profile container Add fb and linkedin image assets to access Add stylings for the profile

--- client/src/containers/containerIndex.js
+++ client/src/containers/containerIndex.js
@@ -1,9 +1,11 @@
 import LandingPage from './LandingPage'
 import OnBoardFlow from './OnBoardFlow'
+import Profile from './Profile'
 import App from './App'
 
 export {
   LandingPage,
   OnBoardFlow,
+  Profile,
   App,
 }
