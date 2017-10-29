28f74edcd9aee664cd2a5517f1a4ae5520410018
Connor Campbell <connorcampbell@connors-mbp.local>
2017-10-29 19:27:24 -0400

    Clean userReducer and epic

--- client/src/redux/modules/user.js
+++ client/src/redux/modules/user.js
@@ -3,30 +3,31 @@ import { ajax } from 'rxjs/observable/dom/ajax';
 // import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/observable/dom/ajax';
 
-import { getUserFullfilled } from './../actions/userActions';
+import { getUserFulfilled } from './../actions/userActions';
 import * as types from '../../utils/types';
 
 const getUserEpic = (action$, state) => {
-  console.log('hit getUserEpic')
+  console.log(state)
   return action$
     .ofType(types.GET_USER)
     .mergeMap(action => 
       ajax.getJSON(`/api/empl/?email=${'connor@tbd.com'}`)
         .map(response => {
-          console.log('inside ajax respoinse')
-          console.log(response)
-          // getUserFullfilled(response)
+          getUserFulfilled(response)
         })
     )
-
 }
 
 const userReducer = (state = {
-  profile: null,
-  skills: [],
-  certifications: [],
+  skills: [
+    { name: 'team work' },
+    { name: 'tool and die making' },
+    { name: 'precision boring' },
+  ],
+  certifications: [
+    { name: 'CNC proficient' },
+  ],
   location: null,
-
 }, action) => {
   const { type, payload } = action;
   switch(type) {
