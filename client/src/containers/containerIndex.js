3dd1e3282a3bb91e3c0ea25c378d163fa9227c68
Connor Campbell <connorcampbell@connors-mbp.local>
2017-11-12 16:39:53 -0500

    Move index contents to containerIndex

--- client/src/containers/containerIndex.js
+++ client/src/containers/containerIndex.js
@@ -1,11 +1,13 @@
+import App from './App'
+import Dashboard from './Dashboard'
 import LandingPage from './LandingPage'
 import OnBoardFlow from './OnBoardFlow'
 import Profile from './Profile'
-import App from './App'
 
 export {
+  App,
+  Dashboard,
   LandingPage,
   OnBoardFlow,
   Profile,
-  App,
 }
