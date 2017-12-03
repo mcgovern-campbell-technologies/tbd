ae56f532b43bec52ee2a1ff24fb4f72f29170ce7
Luke Wilson <wilson.palooza@gmail.com>
2017-12-02 17:17:38 -0800

    change a p element to a div to remove a warning

--- client/src/components/ProfileSideCard.js
+++ client/src/components/ProfileSideCard.js
@@ -12,12 +12,12 @@ function ProfileSideCard(props) {
       <p className="font-size-xl font-bold margin-bottom-none">{name}</p>
       <p className="font-size-m margin-top-none">Machinist</p>
 
-      <p>
+      <div>
               <span>About Rob</span>
 
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum </p>
 
-      </p>
+      </div>
 
       <div className="divider"></div>
       <div>
