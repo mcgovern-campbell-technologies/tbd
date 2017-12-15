5c986ebe520965ca3785738fb2afa926f71c3e97
Connor Campbell <connorcampbell@connors-mbp.local>
2017-12-14 19:11:51 -0500

    Add first rendition of the SideNav bar

--- client/src/containers/SideNavBar.js
+++ client/src/containers/SideNavBar.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
+import { MenuItem } from 'material-ui/Menu';
+import AppBar from 'material-ui/';
+import { withStyles } from 'material-ui/styles';
+
+console.log(classnames);
+
+const drawerWidth = 1000;
+const styles = () => ({
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: drawerWidth,
+  }
+})
+
 
 class SideNavBar extends Component {
   render() {
+    const { classes } = this.props;
+    console.log(classes)
     return (
       <Drawer
+        className={{
+          paper: classnames(classes.drawerPaper)
+        }
+        }
         open={true}
-        zDepth={0}
+        type={'permanent'}
       >
         <MenuItem>Item 1</MenuItem>
         <MenuItem>Item 2</MenuItem>
@@ -16,4 +37,4 @@ class SideNavBar extends Component {
   }
 }
 
-export default SideNavBar
+export default withStyles(styles)(SideNavBar)
