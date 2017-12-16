988caa6abdec5ed331018ad14e9ee1a590ed7ab8
Connor Campbell <connorcampbell@connors-mbp.lan>
2017-12-16 12:12:57 -0500

    Add proptypes and x icon option to SkillChip

--- client/src/components/SkillChip.js
+++ client/src/components/SkillChip.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Chip from 'material-ui/Chip';
 
 class SkillChip extends Component {
 
   render() {
-    const style = {
-      margin: 4,
-      display: 'inline-block'
-    }
     return (
-      <Chip 
-        style={style}
-        label={this.props.name}
-      />
+      <div className='pv2 pr1'>
+        <Chip
+          label={this.props.name}
+          onRequestDelete={this.props.handleRequestDelete}
+        />
+      </div>
     )
   }
+}
+
+SkillChip.propTypes = {
+  name: PropTypes.string.isRequired,
+  handleRequestDelete: PropTypes.func,
 }
 
 export default SkillChip;
