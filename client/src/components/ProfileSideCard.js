bc39d6dc2029ef40541d16a81a18d547deb12cf2
Luke Wilson <wilson.palooza@gmail.com>
2018-01-01 17:42:31 -0800

    tidy up ProfileSideCard and add EditProfileBox component and actions

--- client/src/components/ProfileSideCard.js
+++ client/src/components/ProfileSideCard.js
@@ -1,69 +1,100 @@
-import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import React, {Component} from 'react';
+import Card, {CardActions, CardContent, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Chip from 'material-ui/Chip';
 import ModeEdit from 'material-ui-icons/ModeEdit';
 import IconButton from 'material-ui/IconButton';
 import fbLogo from '../assets/fbLogo.png';
 import lnLogo from '../assets/lnLogo.png';
+import { EditProfileBox } from './componentIndex';
 
-function ProfileSideCard(props) {
-  const { name, email, picture, blurb } = props;
-
-  const styles = {
-    wrapper: {
-      // display: 'flex',
-      // flexWrap: 'wrap',
-      // inline: false
-    },
-    actionIcon: {
-      float: 'right',
-    },
-    profilePosition: {
-      display: 'inline-block',
-      verticalAlign: 'top',
-      whiteSpace: 'normal',
-      paddingRight: '90px'
-    },
-    headerWrapper: {
-      display: 'flex'
-    },
-    header: {
-      flexGrow: '1'
-    },
-    position: {
-      paddingLeft: '16px'
+class ProfileSideCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+      editProfileBoxOpen: false,
     }
+    this.openEditProfileBox = this.openEditProfileBox.bind(this)
   }
 
-  return (
-    <Card>
-      <div style={styles.headerWrapper}>
-        <div className="profile-image">
-          <img src={picture}/>
-        </div>
-        <CardHeader style={styles.header} title={<CardTitle title={name}/>}>
-          <IconButton style={styles.actionIcon}>
-            <ModeEdit/>
-          </IconButton>
-          <div className="profile-position" style={styles.position}>
-            <span>Skilled Machinist</span>
+  openEditProfileBox() {
+    this.setState({ editProfileBoxOpen: true });
+  }
+
+  closeEditProfileBox() {
+    this.setState({ editProfileBoxOpen: false });
+  }
+
+  render () {
+    const { name, email, picture, blurb } = this.props;
+
+    const styles = {
+      wrapper: {
+        // display: 'flex',
+        // flexWrap: 'wrap',
+        // inline: false
+      },
+      actionIcon: {
+        float: 'right',
+      },
+      profilePosition: {
+        display: 'inline-block',
+        verticalAlign: 'top',
+        whiteSpace: 'normal',
+        paddingRight: '90px'
+      },
+      headerWrapper: {
+        display: 'flex'
+      },
+      header: {
+        flexGrow: '1'
+      },
+      position: {
+        paddingLeft: '16px'
+      }
+    }
+
+    return (
+      <div>
+        <EditProfileBox
+          open={this.state.editProfileBoxOpen}
+          closeEditProfileBox={this.closeEditProfileBox.bind(this)}
+          blurb={blurb}
+        />
+        <Card>
+
+          <div style={styles.headerWrapper}>
+            <div className="profile-image">
+              <img src={picture}/>
+            </div>
+
+            <CardHeader style={styles.header} title={name}>
+              <div className="profile-position" style={styles.position}>
+                <span>Skilled Machinist</span>
+              </div>
+            </CardHeader>
+
+            <IconButton onClick={this.openEditProfileBox} style={styles.actionIcon}>
+              <ModeEdit/>
+            </IconButton>
           </div>
-        </CardHeader>
+
+          <CardContent>
+            <div>
+
+              <div>
+                {blurb}
+              </div>
+
+              <div className="divider"></div>
+            </div>
+          </CardContent>
+
+          <CardActions></CardActions>
+        </Card>
       </div>
-      <CardMedia>
-        <div>
-
-          <div>
-            {blurb}
-          </div>
-
-          {/* <div className="divider"></div> */}
-        </div>
-      </CardMedia>
-      <CardActions>
-      </CardActions>
-    </Card>
-  );
+    )
+  }
 }
 
 export default ProfileSideCard;
