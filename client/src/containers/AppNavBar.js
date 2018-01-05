220e05ddb8707991ec742cb483635fdf097e45e2
Luke Wilson <wilson.palooza@gmail.com>
2018-01-04 22:23:55 -0800

    start some changes to fix the top-right menu bug (caused from our upgrade to MUI next)

--- client/src/containers/AppNavBar.js
+++ client/src/containers/AppNavBar.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
-import IconMenu from 'material-ui/Menu';
-import { MenuItem }from 'material-ui/Menu';
+import Menu, { MenuItem }from 'material-ui/Menu';
 import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography'
 // import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
@@ -14,7 +13,7 @@ import { withStyles } from 'material-ui/styles';
 
 const Login = (props) =>  {
   return (
-    <Button 
+    <Button
       onClick={props.login}
     >
       Login
@@ -22,29 +21,50 @@ const Login = (props) =>  {
   );
 }
 
-const Logged = (props) => (
-  <IconMenu
-    iconButtonElement={
-      <IconButton>
-        <Icon/>
-      </IconButton>
-    }
-    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-  >
-    <MenuItem primaryText="Help" />
-    <MenuItem primaryText="Sign out" onClick  ={() => {
-      props.logoutSuccess();
-      props.logout();
-    }}/>
-  </IconMenu>
-);
+const Logged = (props) => {
+  console.log('Logged should show')
+  return (
+    <Menu
+      open={false} //TODO: Need to change this onClick.
+      iconButtonElement={
+        <IconButton>
+          <Icon/>
+        </IconButton>
+      }
+      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+    >
+      <MenuItem primaryText="Help" />
+      <MenuItem primaryText="Sign out" onClick  ={() => {
+        props.logoutSuccess();
+        props.logout();
+      }}/>
+    </Menu>
+  )
+};
 
 class AppNavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      anchorEl: null,
+      open: false,
+    };
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
+
+  handleClick(event) {
+    this.setState({ open: true, anchorEl: event.currentTarget });
+  };
+
+  handleClose() {
+    this.setState({ open: false });
+  };
 
   render() {
     return (
-      <AppBar 
+      <AppBar
         style={this.props.style}
       >
         <Toolbar>
@@ -53,31 +73,31 @@ class AppNavBar extends Component {
           </Typography>
           {
             this.props.isAuthenticated() ? (
-              <Logged 
+              <Logged
                 logoutSuccess={this.props.logoutSuccess}
                 logout={this.props.logout}
-              /> 
+              />
             ) : (
-              <Login 
+              <Login
                 login={this.props.login}
               />
             )
-          } 
+          }
         </Toolbar>
       </AppBar>
     )
   }
 }
         // iconElementRight={this.props.isAuthenticated() ? (
-        //     <Logged 
+        //     <Logged
         //       logoutSuccess={this.props.logoutSuccess}
         //       logout={this.props.logout}
-        //     /> 
+        //     />
         //   ) : (
-        //     <Login 
+        //     <Login
         //       login={this.props.login}
         //     />
         //   )
-        // }   
+        // }
 
 export default AppNavBar
