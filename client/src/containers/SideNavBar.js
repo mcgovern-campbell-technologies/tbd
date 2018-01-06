978fc647604405b11a5b9a510fee9756dd609700
Nanciee <nancyduyi@gmail.com>
2018-01-06 10:29:36 -0500

    58: fixed side nav

--- client/src/containers/SideNavBar.js
+++ client/src/containers/SideNavBar.js
@@ -1,37 +1,19 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
+import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import { MenuItem } from 'material-ui/Menu';
-import AppBar from 'material-ui/';
-import { withStyles } from 'material-ui/styles';
 
-const drawerWidth = 1000;
-const styles = () => ({
-  drawerPaper: {
-    position: 'relative',
-    height: '100%',
-    width: drawerWidth,
-  }
-})
+import sideNavMenuItem from '../constants/sideNav.json';
 
+const SideNavBar = () => (
+  <Drawer
+    type="permanent"
+  >
+    {
+      sideNavMenuItem.map(menuItem =>
+        <MenuItem>{menuItem.name}</MenuItem>
+      )
+    }
+  </Drawer>
+);
 
-class SideNavBar extends Component {
-  render() {
-    const { classes } = this.props;
-    console.log(classes)
-    return (
-      <Drawer
-        className={{
-          paper: classnames(classes.drawerPaper)
-        }
-        }
-        open={false}
-      >
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
-      </Drawer>
-    )
-  }
-}
-
-export default withStyles(styles)(SideNavBar)
+export default SideNavBar
