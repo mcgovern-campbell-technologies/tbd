b27440539661b4bde476ba07d7db5aced6e0aa56
Connor Campbell <connorcampbell@connors-mbp.local>
2018-01-07 18:43:29 -0500

    Add components to the component index

--- client/src/components/componentIndex.js
+++ client/src/components/componentIndex.js
@@ -1,7 +1,11 @@
+import AutoComplete from './AutoComplete';
+import Callback from './Callback';
 import ContactForm from './ContactForm';
+import EditCertificationBox from './EditCertificationBox';
+import EditProfileBox from './EditProfileBox';
+import EditSkillsBox from './EditSkillsBox';
 import LocationForm from './LocationForm';
 import Login from './Login';
-import Callback from './Callback';
 import ProfileSideCard from './ProfileSideCard';
 import SkillChip from './SkillChip';
 import ProfileSectionWrapper from './ProfileSectionWrapper';
@@ -9,19 +13,19 @@ import ProfileSectionListItem from './ProfileSectionListItem';
 import EditSkillsBox from './EditSkillsBox';
 import EditProfileBox from './EditProfileBox';
 import SkillListItem from './SkillListItem';
-import AutoComplete from './AutoComplete';
 
 export {
-  EditSkillsBox,
-  EditProfileBox,
+  AutoComplete,
+  Callback,
   ContactForm,
+  EditCertificationBox,
+  EditProfileBox,
+  EditSkillsBox,
   LocationForm,
   Login,
-  Callback,
   ProfileSideCard,
   SkillChip,
   ProfileSectionWrapper,
   ProfileSectionListItem,
   SkillListItem,
-  AutoComplete,
 }
