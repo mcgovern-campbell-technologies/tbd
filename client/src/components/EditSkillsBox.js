8aac1bbee7050aba7f27cae4c344d74e2b6480d2
Luke Wilson <wilson.palooza@gmail.com>
2018-01-13 21:36:52 -0800

    revert temporary error-squashing attempts

--- client/src/components/EditSkillsBox.js
+++ client/src/components/EditSkillsBox.js
@@ -99,11 +99,7 @@ class EditSkillsBox extends Component {
           </DialogTitle>
           <DialogContent className='w-two-thirds'>
             {
-              !this.props.skills
-              ?
-              null
-              :
-              this.props.skills.map(({ properties, identity }) =>
+              this.props.skills.map(({ properties, identity }) => 
                 <SkillChip
                   { ...properties }
                   key={identity}
