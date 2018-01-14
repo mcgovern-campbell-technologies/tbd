b2d588a4f2a8b53a25db66e086722be4e8878ab3
Luke Wilson <wilson.palooza@gmail.com>
2018-01-13 20:54:23 -0800

    UNIGNORE secrets.js for deployment

---
+++ secrets.js
@@ -0,0 +1,7 @@
+module.exports = {
+  databaseCredentials: {
+    username: 'wilson.palooza@gmail.com',
+    password: 'australia',
+    connection: process.env.neo4jConnectionString || "bolt://localhost"
+  }
+}
