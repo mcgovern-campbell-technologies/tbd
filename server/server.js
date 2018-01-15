a5cce85e330c9e3b2338137894388f7580f8e90e
Luke Wilson <wilson.palooza@gmail.com>
2018-01-14 18:26:12 -0800

    remove comment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ console.log(process.env.DOMAIN)
 console.log('=======================================')
 
 var jwtCheck = require('./auth/auth').jwtCheck;
-//comment
 
 <<<<<<< HEAD
 const GraphApi = require('./database/graphApi.js')
