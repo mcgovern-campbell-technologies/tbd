5c83ba59e3d23701ad3dab40e0f31ab4a530ae9c
Connor Campbell <connorcampbell@connors-mbp.local>
2018-01-20 14:52:54 -0500

    Add the new experience router and add it to contractor router

---
+++ server/routers/contractorExperienceRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+
+router.get('/', (req, res) => {
+  const { query, graphApi } = req;
+  const { identity } = query;
+  graphApi.getContractorExpirience(identity)
+    .then(result => {
+      res.send(result);
+    })
+})
+
+router.post('/', (req, res) => {
+  const { body, query, graphApi } = req;
+  const { identity } = query;
+
+  graphApi.addContractorExpirience(identity, body)
+    .then(result => {
+      res.send(result);
+    })
+})
+
+module.exports = router
