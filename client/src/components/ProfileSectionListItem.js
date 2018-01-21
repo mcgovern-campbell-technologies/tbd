a373b458eae7bb011aaab96aed90f102b04498be
Connor Campbell <connorcampbell@connors-mbp.local>
2018-01-20 20:01:04 -0500

    Fix the ProfileSectionListItems weird layout

--- client/src/components/ProfileSectionListItem.js
+++ client/src/components/ProfileSectionListItem.js
@@ -20,15 +20,15 @@ class ProfileSectionListItem extends Component {
     this.props.openEditBox(this.props.node);
   }
   render() {
-    const { name, location, institution } = this.props.node;
+    const { name, location, institution, date } = this.props.node.properties;
     return (
       <div className='mb2'>
-        <div className='dib fl mr3'>
+        <div className='dib mr3'>
           <Avatar 
               style={{ height: 60, width: 60 }}
               sizes='10'
             >
-            C
+            { name[0] }
           </Avatar>
         </div>
         <div className='dib'>
@@ -49,7 +49,7 @@ class ProfileSectionListItem extends Component {
             <ModeEdit />
           </IconButton>
           <Typography>
-            when to when
+            { date }
           </Typography>
         </div>
       </div>
@@ -57,33 +57,6 @@ class ProfileSectionListItem extends Component {
   }
 }
 
-/*
-<div>
-        <div className='fl ph2'>
-          <Avatar 
-            style={{ height: 70, width: 70 }}
-            sizes='10'
-          >
-            C
-          </Avatar>
-        </div>
-        <div className='fl'>
-          <Typography>
-            { this.props.title }
-          </Typography>
-        </div>
-        <div className='fr'>
-          <IconButton
-            onClick={this.handleOpenEditBox}
-          >
-            <ModeEdit />
-          </IconButton>
-          <Typography>
-            {`${'hellp'} - ${'goodbyes'}`}
-          </Typography>
-        </div>
-      </div>
-*/
 ProfileSectionListItem.propTypes = {
   title: PropTypes.string,
   institution: PropTypes.string,
