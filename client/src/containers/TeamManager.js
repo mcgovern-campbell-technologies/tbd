796f3d962145d5fa0f853e210081119fe46f28ac
Connor Campbell <connorcampbell@connors-mbp.local>
2018-01-25 19:12:34 -0500

    Add TeamManagerWrapper to TeamManager

--- client/src/containers/TeamManager.js
+++ client/src/containers/TeamManager.js
@@ -5,19 +5,32 @@ import {
 
 } from './containerIndex';
 import {
-
+  TeamManagerWrapper,
 } from './../components/componentIndex';
 
 class TeamManager extends Component {
   constructor(props) {
     super(props);
-    console.log(props)
+
+    this.state = {
+      AddTeamBoxOpen: false,
+    }
+
+    this.toggleAddTeamBox = this.toggleAddTeamBox.bind(this)
   }
+
+  toggleAddTeamBox() {
+    this.setState({AddTeamBoxOpen: !this.state.AddTeamBoxOpen})
+    console.log(this.state)
+  }
+
   render() {
     return (
-      <div>
-        hi bidge
-      </div>
+      <TeamManagerWrapper
+        openAddTeamBox={this.toggleAddTeamBox}
+      >
+        
+      </TeamManagerWrapper>
     )
   }
 }
