f0371d96c484cd4f8f47e1fb0ec82e519b00938a
Luke Wilson <wilson.palooza@gmail.com>
2018-02-18 22:41:19 -0800

    create route for /api/team/experience

--- server/routers/teamRouter.js
+++ server/routers/teamRouter.js
@@ -3,14 +3,38 @@ const router = express.Router();
 const _ = require('lodash');
 
 
+// Get a team by ID: GET /api/team
 // router.get('/', (req, res) => {
 //
 // });
 
+// Create a team and add to project: POST /api/team
 router.post('/', (req, res) => {
   const { body, query } = req;
 
   req.graphApi.createTeam(body)
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      res.send(err)
+    });
+});
+
+// POST /api/team/experience
+router.post('/experience', (req, res) => {
+  const { body, query } = req;
+
+  if (body.remove) {
+    req.graphApi.removeExperienceFromTeam(body)
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        res.send(err)
+      });
+  }
+  req.graphApi.addExperienceToTeam(body)
     .then(result => {
       res.send(result);
     })
