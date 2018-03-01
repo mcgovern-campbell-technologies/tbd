fdd5503ffa82a70f924ce0ba7f50985e00f662cc
Luke Wilson <wilson.palooza@gmail.com>
2018-02-28 22:17:48 -0800

    add positionRouter to server

--- server/routers/routersIndex.js
+++ server/routers/routersIndex.js
@@ -2,6 +2,7 @@ const certificationRouter = require('./certificationRouter');
 const contractorRouter = require('./contractorRouter');
 const contractorSkillsRouter = require('./contractorSkillsRouter');
 const locationRouter = require('./locationRouter');
+const positionRouter = require('./positionRouter');
 const projectRouter = require('./projectRouter');
 const skillRouter = require('./skillRouter');
 const teamRouter = require('./teamRouter');
@@ -11,6 +12,7 @@ module.exports = {
   contractorRouter,
   contractorSkillsRouter,
   locationRouter,
+  positionRouter,
   projectRouter,
   skillRouter,
   teamRouter,
