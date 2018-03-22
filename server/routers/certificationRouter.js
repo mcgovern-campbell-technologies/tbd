46501303dee7daad910191891f2f5f088852b84b
Luke Wilson <wilson.palooza@gmail.com>
2018-03-21 22:09:01 -0700

    refactor all of our server routers

--- server/routers/certificationRouter.js
+++ server/routers/certificationRouter.js
@@ -1,17 +1,12 @@
 //  /api/certification
-
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   const { queryString } = req.query
   req.graphApi.getParentNodeList(queryString, 'Certification')
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      res.send(err);
-    })
+    .then(result => res.send(result))
+    .catch(err => res.send(err))
 });
 
 module.exports = router;
--- server/routers/positionRouter.js
+++ server/routers/positionRouter.js
@@ -1,21 +1,14 @@
 //  /api/position
-
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 
+// Create a position: POST /api/position
 router.post('/', (req, res) => {
   const { body, query } = req;
-
   req.graphApi.createPosition(body)
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      res.send(err)
-    });
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
 });
-
-
 
 module.exports = router
