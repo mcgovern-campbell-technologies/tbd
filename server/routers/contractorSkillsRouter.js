28b3091760d0481a75bead64fd2a4b6af84d91cb
Luke Wilson <wilson.palooza@gmail.com>
2018-03-21 21:48:38 -0700

    remove console.logs

--- server/routers/contractorSkillsRouter.js
+++ server/routers/contractorSkillsRouter.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const { identity } = req.query;
-  console.log('in contractorSkillsRouter get', identity)
   req.graphApi.getContractorSkills(identity)
     .then(result => {
       res.send(result);
@@ -15,10 +14,8 @@ router.post('/', (req, res) => {
 
   const { body, query } = req;
   const { identity } = query;
-  console.log(body);
   req.graphApi.addSkillToContractor(identity, body)
     .then(result => {
-      console.log(result)
       return req.graphApi.getContractorSkills(identity)
     })
     .then(result => {
