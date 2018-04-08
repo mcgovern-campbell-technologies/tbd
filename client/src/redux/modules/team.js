81ac94be79de2fe293bdee79780aac496fb6f223
Luke Wilson <wilson.palooza@gmail.com>
2018-04-07 19:20:20 -0700

    revert team.js changes

--- client/src/redux/modules/team.js
+++ client/src/redux/modules/team.js
@@ -9,19 +9,16 @@ import {
   GET_TEAM_FULFILLED,
 } from '../../utils/types';
 
-import { getAllTeams } from '../../core/services/teams';
-
 const DOMAIN = window.location.host || 'localhost'
-
 
 const addTeamEpic = (action$, state) => {
   return action$
     .ofType(ADD_TEAM)
     .mergeMap(
       action => {
-        return getAllTeams()
+        return ajax.post(`http://${DOMAIN}:4000/api/team`, action.payload)
           .map(({response}) => {
-            return getTeamFulfilled(response.data)
+            return getTeamFulfilled(response)
           })
       }
     )
