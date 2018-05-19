f710824172b56eb23239fc797be6233b7ec2e7c3
Connor Campbell <connorcampbell@connors-mbp.local>
2018-05-19 16:09:56 -0400

    Add an icon to the cells

--- client/src/components/TeamSummaryRoleRow.js
+++ client/src/components/TeamSummaryRoleRow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { TableCell, TableRow } from 'material-ui/Table';
+import ModeEdit from 'material-ui-icons/ModeEdit';
 
 export default function TeamSummaryRoleRow(props) {
   const { id, type, skillLevel, weekDays, time, requested, filled } = props
@@ -12,6 +13,7 @@ export default function TeamSummaryRoleRow(props) {
       <TableCell>{time}</TableCell>
       <TableCell>{requested}</TableCell>
       <TableCell>{filled}</TableCell>
+      <TableCell><ModeEdit /></TableCell>
     </TableRow>
   );
 }
