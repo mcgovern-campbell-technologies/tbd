dfdf53f1180804edb0b84ec691d339944465f984
Connor Campbell <connorcampbell@connors-mbp.local>
2018-05-20 18:19:34 -0400

    Change the react function to a class for methods and state

--- client/src/components/TeamSummaryRoleRow.js
+++ client/src/components/TeamSummaryRoleRow.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import { TableCell, TableRow } from 'material-ui/Table';
 import ModeEdit from 'material-ui-icons/ModeEdit';
+import IconButton from 'material-ui/IconButton'
 
-export default function TeamSummaryRoleRow(props) {
-  const { id, type, skillLevel, weekDays, time, requested, filled } = props
-  return (
-    <TableRow key={id}>
-      <TableCell>{type} - {skillLevel}</TableCell>
-      <TableCell>{skillLevel}</TableCell>
-      <TableCell>{weekDays}</TableCell>
-      <TableCell>{time}</TableCell>
-      <TableCell>{requested}</TableCell>
-      <TableCell>{filled}</TableCell>
-      <TableCell><ModeEdit /></TableCell>
-    </TableRow>
-  );
+export default class TeamSummaryRoleRow extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovering: false
+    }
+  }
+
+  mouseOver() {
+    this.setState({ hovering: true })
+  }
+
+  mouseOut() {
+    this.setState({ hovering: false } )
+  }
+
+  render() {
+    const { node, handleEditButtonClick } = this.props;
+    const { id, type, skillLevel, weekDays, time, requested, filled } = node
+    return (
+      <TableRow 
+        onMouseOver={this.mouseOver.bind(this)}
+        onMouseOut={this.mouseOut.bind(this)}
+        key={id}
+      >
+        <TableCell>{type} - {skillLevel}</TableCell>
+        <TableCell>{skillLevel}</TableCell>
+        <TableCell>{weekDays}</TableCell>
+        <TableCell>{time}</TableCell>
+        <TableCell>{requested}</TableCell>
+        <TableCell>{filled}</TableCell>
+        <TableCell>
+          <IconButton
+            disabled={!this.state.hovering}
+            onClick={handleEditButtonClick}
+          >
+            <ModeEdit />
+          </IconButton> 
+          
+        </TableCell>
+      </TableRow>
+    );
+  }
+}
+
+TeamSummaryRoleRow.propTypes = {
+  node: PropTypes.object.isRequired,
+  handleEditButtonClick: PropTypes.func.isRequired,
 }
