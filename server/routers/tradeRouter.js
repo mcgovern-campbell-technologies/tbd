cca191e0461660081a8829dcab08f2e1f7335c4b
Luke Wilson <wilson.palooza@gmail.com>
2018-06-09 21:51:12 -0700

    rename positionRouter to tradeRouter

---
+++ server/routers/tradeRouter.js
@@ -0,0 +1,30 @@
+//  /api/trade
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+
+// Get all trades: GET /api/trade
+router.get('/', (req, res) => {
+  const { body, query } = req;
+  req.graphApi.getAllTrades(body)
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+});
+
+// Create a trade: POST /api/trade
+router.post('/', (req, res) => {
+  const { body, query } = req;
+  req.graphApi.createTrade(body)
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+});
+
+// Get all trades: GET /api/trade/positionLevels
+router.get('/positionLevels', (req, res) => {
+  req.graphApi.getPositionLevels()
+    .then(result => res.send(result))
+    .catch(err => res.send(err));
+});
+
+
+module.exports = router
